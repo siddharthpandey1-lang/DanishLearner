1355434d418b0fbb35b381600991d9416573f7e8
Siddhart <Siddharth.pandey1@gmail.com>
2025-10-07 19:16:12 +0530

    Level3+Quiz

---
+++ QuizLevel.py
@@ -0,0 +1,19 @@
+print("Quiz time lets check your memory")
+print("what do we say dog in danish")
+hund = input()
+if hund.lower() == "hund":
+    print("correct")
+else:
+    print("incorrect")
+print("What do we say family in danish")
+Familie = input()
+if Familie.lower() == "familie":
+	print("correct")
+else:
+	print("incorrect")
+print("what do we say friends in danish")
+Venner = input()
+if Venner.lower() == "venner":
+    print("correct")
+else:
+    print("incorrect")
